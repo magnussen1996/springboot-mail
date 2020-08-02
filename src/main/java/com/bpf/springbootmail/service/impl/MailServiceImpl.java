ad3d3db18416445f9d857123883c4872e42f5339
magnussen1996 <1193378607@qq.com>
2020-08-02 17:00:35 +0800

    部分代码优化

--- src/main/java/com/bpf/springbootmail/service/impl/MailServiceImpl.java
+++ src/main/java/com/bpf/springbootmail/service/impl/MailServiceImpl.java
@@ -1,10 +1,6 @@
 package com.bpf.springbootmail.service.impl;
 
-import java.io.File;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-
+import com.bpf.springbootmail.service.MailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,54 +11,72 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import com.bpf.springbootmail.service.MailService;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.util.Arrays;
 
 @Service
 public class MailServiceImpl implements MailService {
-    
+
     private final Logger logger = LoggerFactory.getLogger(MailServiceImpl.class);
 
     @Value("${spring.mail.username}")
     private String from;
-    
+
     @Autowired
     private JavaMailSender mailSender;
+
     @Override
     public void sendSimpleMail(String to, String subject, String content) {
-        
+
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);//收信人
-        message.setSubject(subject);//主题
-        message.setText(content);//内容
-        message.setFrom(from);//发信人
-        
+        //收信人
+        message.setTo(to);
+        //主题
+        message.setSubject(subject);
+        //内容
+        message.setText(content);
+        //发信人
+        message.setFrom(from);
+
         mailSender.send(message);
     }
+
     @Override
     public void sendHtmlMail(String to, String subject, String content) {
-        
+
         logger.info("发送HTML邮件开始：{},{},{}", to, subject, content);
         MimeMessage message = mailSender.createMimeMessage();
-        
+
         MimeMessageHelper helper;
         try {
             helper = new MimeMessageHelper(message, true);
             helper.setFrom(from);
             helper.setTo(to);
             helper.setSubject(subject);
-            helper.setText(content, true);//true代表支持html
+            //true代表支持html
+            helper.setText(content, true);
             mailSender.send(message);
             logger.info("发送HTML邮件成功");
         } catch (MessagingException e) {
             logger.error("发送HTML邮件失败：", e);
-        }    
+        }
     }
+
     @Override
-    public void sendAttachmentMail(String to, String subject, String content, String filePath) {
-        
-        logger.info("发送带附件邮件开始：{},{},{},{}", to, subject, content, filePath);
+    public void sendAttachmentMails(String to, String subject, String content, String[] filePaths) {
+        logger.info("发送邮件开始，发送至：{}；主题：{}；内容：{}；附件路径：{}", to, subject, content, filePaths);
+        if (isBlank(to)) {
+            logger.warn("无收件人，发送邮件失败");
+            return;
+        }
+
+        if (isBlank(subject)) {
+            subject = "未命名主题";
+        }
+
         MimeMessage message = mailSender.createMimeMessage();
-        
         MimeMessageHelper helper;
         try {
             helper = new MimeMessageHelper(message, true);
@@ -70,23 +84,44 @@ public class MailServiceImpl implements MailService {
             helper.setTo(to);
             helper.setSubject(subject);
             helper.setText(content, true);
-            FileSystemResource file = new FileSystemResource(new File(filePath));
-            String fileName = file.getFilename();
-            helper.addAttachment(fileName, file);//添加附件，可多次调用该方法添加多个附件  
-            mailSender.send(message);
-            logger.info("发送带附件邮件成功");
         } catch (MessagingException e) {
-            logger.error("发送带附件邮件失败", e);
+            logger.error("设置邮件辅助器异常", e);
+            return;
         }
 
-        
+        // 添加附件
+        if (filePaths != null) {
+            Arrays.stream(filePaths)
+                    .filter(path -> !isBlank(path))
+                    .map(path -> new FileSystemResource(new File(path)))
+                    .forEach(file -> {
+                        String fileName = file.getFilename();
+                        if (isBlank(fileName)) {
+                            fileName = "unknown";
+                        }
+                        try {
+                            helper.addAttachment(fileName, file);
+                        } catch (MessagingException e) {
+                            logger.error("添加附件异常", e);
+                        }
+                    });
+        }
+
+        mailSender.send(message);
+        logger.info("发送邮件成功");
     }
+
+    @Override
+    public void sendAttachmentMail(String to, String subject, String content, String filePath) {
+        sendAttachmentMails(to, subject, content, new String[]{filePath});
+    }
+
     @Override
     public void sendInlineResourceMail(String to, String subject, String content, String rscPath, String rscId) {
-        
+
         logger.info("发送带图片邮件开始：{},{},{},{},{}", to, subject, content, rscPath, rscId);
         MimeMessage message = mailSender.createMimeMessage();
-        
+
         MimeMessageHelper helper;
         try {
             helper = new MimeMessageHelper(message, true);
@@ -95,12 +130,17 @@ public class MailServiceImpl implements MailService {
             helper.setSubject(subject);
             helper.setText(content, true);
             FileSystemResource res = new FileSystemResource(new File(rscPath));
-            helper.addInline(rscId, res);//重复使用添加多个图片
+            //重复使用添加多个图片
+            helper.addInline(rscId, res);
             mailSender.send(message);
             logger.info("发送带图片邮件成功");
         } catch (MessagingException e) {
             logger.error("发送带图片邮件失败", e);
         }
+    }
+
+    private boolean isBlank(String string) {
+        return string == null || "".equals(string.trim());
     }
 
 }
--- src/test/java/com/bpf/springbootmail/service/ServiceTest.java
+++ src/test/java/com/bpf/springbootmail/service/ServiceTest.java
@@ -16,46 +16,58 @@ public class ServiceTest {
     private MailService mailService;
     @Autowired
     private TemplateEngine templateEngine;
-    
+
+    private static final String EMAIL_ADDRESS = "15131260825@163.com";
+
     /**
      * 发送简单纯文本邮件
      */
     @Test
     public void sendSimpleMail() {
-        mailService.sendSimpleMail("receiver@email.com", "发送邮件测试", "大家好，这是我用springboot进行发送邮件测试");
+        mailService.sendSimpleMail(EMAIL_ADDRESS, "发送邮件测试", "大家好，这是我用springboot进行发送邮件测试");
     }
-    
+
     /**
      * 发送HTML邮件
      */
     @Test
     public void sendHtmlMail() {
         String content = "<html><body><h3><font color=\"red\">" + "大家好，这是springboot发送的HTML邮件" + "</font></h3></body></html>";
-        mailService.sendHtmlMail("receiver@email.com", "发送邮件测试", content);
+        mailService.sendHtmlMail(EMAIL_ADDRESS, "发送邮件测试", content);
     }
-    
+
     /**
      * 发送带附件的邮件
      */
     @Test
     public void sendAttachmentMail() {
         String content = "<html><body><h3><font color=\"red\">" + "大家好，这是springboot发送的HTML邮件，有附件哦" + "</font></h3></body></html>";
-        String filePath = "your file path";
-        mailService.sendAttachmentMail("receiver@email.com", "发送邮件测试", content, filePath);
+        String filePath = "./attach.txt";
+        mailService.sendAttachmentMail(EMAIL_ADDRESS, "发送邮件测试", content, filePath);
     }
-    
+
+    /**
+     * 发送带多个附件的邮件
+     */
+    @Test
+    public void sendAttachmentMails() {
+        String content = "<html><body><h3><font color=\"red\">" + "大家好，这是springboot发送的HTML邮件，有附件哦" + "</font></h3></body></html>";
+        String[] filePaths = {"./attach.txt", "./attach1.txt"};
+        mailService.sendAttachmentMails(EMAIL_ADDRESS, "发送邮件测试", content, filePaths);
+    }
+
     /**
      * 发送带图片的邮件
      */
     @Test
     public void sendInlineResourceMail() {
-        String rscPath = "your picture path";
+        String rscPath = "./photo.png";
         String rscId = "skill001";
         String content = "<html><body><h3><font color=\"red\">" + "大家好，这是springboot发送的HTML邮件，有图片哦" + "</font></h3>"
-                         + "<img src=\'cid:" + rscId + "\'></body></html>";
-        mailService.sendInlineResourceMail("receiver@email.com", "发送邮件测试", content, rscPath, rscId);
+                + "<img src=\'cid:" + rscId + "\'></body></html>";
+        mailService.sendInlineResourceMail(EMAIL_ADDRESS, "发送邮件测试", content, rscPath, rscId);
     }
-    
+
     /**
      * 指定模板发送邮件
      */
@@ -65,7 +77,7 @@ public class ServiceTest {
         Context context = new Context();
         context.setVariable("id", "001");
         String emailContent = templateEngine.process("emailTemplate", context);
-        
-        mailService.sendHtmlMail("receiver@email.com", "这是一个模板文件", emailContent);
+
+        mailService.sendHtmlMail(EMAIL_ADDRESS, "这是一个模板文件", emailContent);
     }
 }
